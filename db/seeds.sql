0c48f7da786bdbd4bbd48246d8495d814af68e09
Stu Eliades <stuart.eliades@gmail.com>
2019-09-02 00:18:21 -0400

    created folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Royale With Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Sourdough Patty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Big Fat Burger');
